ada3221e9d5738dee889a11f475f29e23f129e52
TamaraBarrado <barradotamara@gmail.com>
2024-01-26 15:05:50 +0100

    Recipe list and detail

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import HomeScreen from './screens/HomeScreen.vue';
 import DetailScreen from './screens/DetailScreen.vue';
+import CreateRecipe from './screens/CreateRecipe.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomeScreen },
-    { path: '/recipes/:id', component: DetailScreen, props: true }
+    { path: '/recipes/:id', component: DetailScreen, props: true },
+    { path: '/recipes/create', component: CreateRecipe }
   ]
 });
 
